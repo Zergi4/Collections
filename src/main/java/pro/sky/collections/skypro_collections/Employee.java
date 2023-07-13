d742e45d0005a1afd770b4156410b549d463d7ae
Serg <ksyssau@ya.ru>
2023-07-13 21:03:06 +0300

    начало

---
+++ src/main/java/pro/sky/collections/skypro_collections/Employee.java
@@ -0,0 +1,57 @@
+package pro.sky.collections.skypro_collections;
+
+
+
+public class Employee {
+    private final String fio;
+    private int department;
+    private float salary;
+    private static int counter;
+    private final int id;
+
+    //Конструктор
+    public Employee(String fio, int department, float salary) {
+        this.fio = fio;
+        this.department = department;
+        this.salary = salary;
+        id = ++counter;
+    }
+
+    //Геттеры
+    public String getFio() {
+        return fio;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    //Сетттеры
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+
+    @Override
+    public String toString() {
+        return "" +
+                "id = " + id +
+                " ФИО = '" + fio + '\'' +
+                ", Отдел = " + department +
+                ", Зарплата = " + salary +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
